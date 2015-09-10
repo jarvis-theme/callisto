3182ec90d9419e35080dc77cb0b6c83a8f2e0421
yogaswara <gistha.yogaswara@gmail.com>
2015-09-10 17:21:40 +0700

    update meta tag fb

--- partials/seostuff.blade.php
+++ partials/seostuff.blade.php
@@ -4,7 +4,7 @@
 <meta name="description" content="{{$description}}">
 <meta name="keywords" content="{{$keywords}}">
 <meta name="rating" content="general">
-<meta name="author" content="jarvis-store">
+<!-- <meta name="author" content="jarvis-store"> -->
 <meta name="robots" content="index, follow">
 <meta http-equiv="classification" content="Toko online">
 <meta name="url" content="{{URL::current()}}">
@@ -13,8 +13,8 @@
 <meta name="DC.Subject" content="{{$keywords}}">
 <meta name="DC.Description" content="{{$description}}">
 <meta name="theme_path" content="{{theme_path()}}">
-<meta property="og:url"           content="{{URL::to('/')}}" />
-<meta property="og:type"          content="{{URL::full()}}" />
+<meta property="og:url"           content="{{URL::full()}}" />
+<meta property="og:type"          content="product" />
 <meta property="og:title"         content="{{$title}}" />
 <meta property="og:description"   content="{{$description}}" />
 <meta property="og:image"         content="{{@$img!='' ? product_image_url($img):''}}" />
